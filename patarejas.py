7ebae8eca59d78f63fa9e940209d5e21c9bad06c
JonasSatkauskas <joncius224@gmail.com>
2020-12-16 22:34:30 +0200

    pirmas

---
+++ patarejas.py
@@ -0,0 +1,5 @@
+"""Ši programa patars ką veikti kai nėra ką veikti.
+Bus galima pasirinkti iš atitinkamų kategorijų kurios turės veiklas"""
+
+# DICTIONARY KURIAME YRA VEIKLU KATEGORIJOS IR VEIKLOS
+
