473b3f72919970fc9349bb28bec1f3f5861f8765
JonasSatkauskas <joncius224@gmail.com>
2020-12-17 02:22:41 +0200

    Dictionary papildymas

--- patarejas.py
+++ patarejas.py
@@ -3,4 +3,14 @@ Bus galima pasirinkti iš atitinkamų kategorijų kurios turės veiklas"""
 
 # DICTIONARY KURIAME YRA VEIKLU KATEGORIJOS IR VEIKLOS
 
+kategorijos_veiklos = {
+    1: 'Pramogos',
+    2: 'Mokslai',
+    3: 'Namų ruoša',
+    4: 'Muzika',
+    5: 'Socializavimasis'
+}
+
 # FUNKCIJOS
+
+
